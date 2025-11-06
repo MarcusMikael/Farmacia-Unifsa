fc1103040e7ce9b33954a8bd5aa16a26f95d4bcb
Marcus Mikael <marcusmikael2@gmail.com>
2025-11-06 19:35:21 -0300

    Configuração do banco e geração de PDF

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ import io
 # ======================= CONFIGURAÇÃO SUPABASE =======================
 
 load_dotenv()
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
